5e9c153ace074bd7bbbda7cbcdb4236d3de25079
microtv <macuser@pete-s-mac-mini.local>
2014-07-12 11:41:50 +0200

    Ver 0.0.36

--- script.tvguidemicro/addon.py
+++ script.tvguidemicro/addon.py
@@ -30,7 +30,7 @@ xbmc.Player().stop
 ADDON       = xbmcaddon.Addon(id = 'script.tvguidemicro')
 HOME        = ADDON.getAddonInfo('path')
 TITLE       = 'Micro TV Guide'
-VERSION     = '0.0.36'
+VERSION     = '0.0.37'
 addon       = xbmcaddon.Addon()
 addonid     = addon.getAddonInfo('id')
 versioninfo = addon.getAddonInfo('version')
